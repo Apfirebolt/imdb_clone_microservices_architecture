eab23b20c30813605c86fcc73ff7e481d7d8f794
Apfirebolt <aspper20@gmail.com>
2026-08-15 22:03:56 +0530

    feat: consumer for microservices

--- movies_service/main.js
+++ movies_service/main.js
@@ -1,8 +1,10 @@
 import app from "./app.js";
 import connectDB from "./config/db.js";
+import { runUserSyncConsumer } from "./utils/consumer.js";
 // import { connectProducer } from "./server/utils/kafkaConnect.js";
 
 connectDB();
+runUserSyncConsumer();
 // connectProducer();
 
 const PORT = process.env.PORT || 4000;
